61f6736b49a90b30484b6e90f381c11c07fb4be3
Poojan Patel <poojan.patel@cae.com>
2023-09-18 15:26:31 +0530

    Setup menu item in toolbar.

--- app/src/main/java/com/imgurgallery/HomeActivity.kt
+++ app/src/main/java/com/imgurgallery/HomeActivity.kt
@@ -1,16 +1,17 @@
 package com.imgurgallery
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.imgurgallery.ui.theme.ImgurGalleryTheme
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return super.onPrepareOptionsMenu(menu)
     }
 }
